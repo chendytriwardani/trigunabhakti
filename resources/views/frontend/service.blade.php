b372a544aef2bd3440e4a73c9b51b2736c5a47f4
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2023-01-17 13:34:39 +0700

    navbar

--- resources/views/frontend/service.blade.php
+++ resources/views/frontend/service.blade.php
@@ -3,19 +3,21 @@
             <div class="row justify-content-around">
                 @foreach ($services as $srv)
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
-                    <div class="icon-box icon-box-pink">
-                        @if ($srv->title == 'SD Tri Guna Bhakti')
+                    <a href="/blog/{{ $srv->slug }}">
+                        <div class="icon-box icon-box-pink">
+                            @if ($srv->title == 'SD Tri Guna Bhakti')
                             <div class="icon"><img src="{{ asset('images/logo/sd.png') }}" class="img-circle elevation-2" alt="Logo" width="40"></i></div>
-                        @elseif ($srv->title == 'SMP Tri Guna Bhakti')
+                            @elseif ($srv->title == 'SMP Tri Guna Bhakti')
                             <div class="icon"><img src="{{ asset('images/logo/smp.png') }}" class="img-circle elevation-2" alt="Logo" width="40"></i></div>
-                        @elseif ($srv->title == 'SMK Tri Guna Bhakti')
+                            @elseif ($srv->title == 'SMK Tri Guna Bhakti')
                             <div class="icon"><img src="{{ asset('images/logo/smk.png') }}" class="img-circle elevation-2" alt="Logo" width="40"></i></div>
-                        @else
+                            @else
                             <div class="icon"><i class="bx bxs-school"></i></div>
-                        @endif
-                        <h4 class="title"><a href="/blog/{{ $srv->slug }}">{{ $srv->title }}</a></h4>
-                        <p class="description">{{ $srv->excerpt }}</p>
-                    </div>
+                            @endif
+                            <h4 class="title"><a href="/blog/{{ $srv->slug }}">{{ $srv->title }}</a></h4>
+                            <p class="description"><a style="color: black" href="/blog/{{ $srv->slug }}">{{ $srv->excerpt }}</a></p>
+                        </div>
+                    </a>
                 </div>
             @endforeach
         </div>
--- resources/views/layouts/frontend/nav.blade.php
+++ resources/views/layouts/frontend/nav.blade.php
@@ -1,26 +1,23 @@
 <nav id="navbar" class="navbar">
     <ul>
       <li><a class="active " href="/">Home</a></li>
+      <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a href="#">Sejarah</a></li>
+          <li><a href="#">Visi Misi</a></li>
+          <li><a href="#">Program unggulan</a></li>
+          <li class="dropdown"><a href="#"><span>Pendidikan</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="#">SD</a></li>
+              <li><a href="#">SMP</a></li>
+              <li><a href="#">SMK</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
       <li><a href="{{ route('blog.index') }}">Article </a></li>
       <li><a href="{{ route('gallery.show') }}">Gallery</a></li>
       <li><a href="{{ url('about') }}">About</a></li>
-      <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-        <ul>
-          <li><a href="#">Drop Down 1</a></li>
-          <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-            <ul>
-              <li><a href="#">Deep Drop Down 1</a></li>
-              <li><a href="#">Deep Drop Down 2</a></li>
-              <li><a href="#">Deep Drop Down 3</a></li>
-              <li><a href="#">Deep Drop Down 4</a></li>
-              <li><a href="#">Deep Drop Down 5</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Drop Down 2</a></li>
-          <li><a href="#">Drop Down 3</a></li>
-          <li><a href="#">Drop Down 4</a></li>
-        </ul>
-      </li>
       <li class="nav-item">
         @auth
         <li class="nav-item {{ request()->is('gallery')?'active':'' }}">
